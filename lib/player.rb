138b3a7e3a47a549f280c8bc9abc02e5a36c65c2
Ryan Brady <ryanbrady369@gmail.com>
2018-11-20 02:09:49 +0000

    automatically backed up by learn

--- lib/player.rb
+++ lib/player.rb
@@ -5,4 +5,20 @@ class Player
     @token = token
   end
 
+  # def get
+  #   token.one
+  # end
 end 
+
+# class Token1
+#   def one
+#     blah
+#   end
+# end
+# 
+# class Token2
+#   def one
+#     blach
+#   end
+# end
+# Player.new()
